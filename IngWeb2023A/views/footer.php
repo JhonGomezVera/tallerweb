6cb5104f7a6606e0c63fc69d161e690c3970063c
JhonGomezVera <134179270+JhonGomezVera@users.noreply.github.com>
2023-05-25 17:18:33 -0500

    Delete IngWeb2023A directory

--- IngWeb2023A/views/footer.php
+++
@@ -1,6 +0,0 @@
-<h3>© 2023 Todos los derechos reservados.
-Dirección: calle 1234
-Teléfono: 123456789
-Correo electrónico: notengo@notengo.com
-Sitio web: notengo.com.co
-Síguenos en las redes sociales: notengo</h3>
--- IngWeb2023A/views/header.php
+++
@@ -1,6 +0,0 @@
-<div class="container logo-nav-container">
-  <img src="fotos/descarga.jpg" alt="Logo" width="200" height="100">
-  <?php 
-    include "views/modules/navegacion.php";
-  ?>
-</div>
